2f753df0a98b51d7ef152291309cf107972e25e7
Michal Bugno <michal@bugno.net>
2009-01-08 01:17:52 +0100

    getopt and main

--- main.c
+++ main.c
@@ -4,65 +4,98 @@
 #include "main.h"
 #include "system.h"
 
-int eradicate(int pid)
+int kill_master(const char *filename)
+{
+	FILE *file;
+	int pid;
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		perror("Couldn't open file");
+		return -1;
+	}
+
+	fscanf(file, "%d", &pid);
+	if (eradicate(pid) != 0)
+	{
+		printf("Couldn't kill process %d\n", pid);
+		return -1;
+	}
+	fclose(file);
+	unlink(filename);
+	return 1;
+}
+
+int eradicate(const int pid)
 {
 	return kill((pid_t)pid, SIGKILL) != 0;
 }
 
 int main(int argc, char **argv)
 {
-	pid_t pid;
-	FILE *f;
-	
-	pid = fork();
-
 	dispatch_from_args(argc, argv);
-
-	pid = fork();
-	if (pid < 0) exit(1);
-	if (pid > 1)
-	{
-		printf("%d\n", pid);
-		exit(0);
-	}
 	return 0;
 }
 
 int dispatch_from_args(int argc, char **argv)
 {
 	int c;
-	while ((c = getopt(argc, argv, "k:")) != -1)
+
+	if (argc == 1)
 	{
-		switch (c)
+		do_run();
+		return 1;
+	} else
+	{
+		while ((c = getopt(argc, argv, "k")) != -1)
 		{
-			case 'k':
-				break;
+			switch (c)
+			{
+				case 'k':
+					printf("Killing server...\n");
+					kill_master("aids.pid");
+					break;
 
-			case '?':
-				printf("Usage: %s [-k]\n", argv[0]);
-				break;
-
-			default:
-				do_run();
-				break;
+				case '?':
+					printf("Usage: %s [-k]\n", argv[0]);
+					break;
+			}
 		}
+		return c;
 	}
 
-	return c;
 }
 
 void do_run(void)
 {
-	int i, r;
+	pid_t pid;
 	struct network_traffic traffic;
 	struct load_average load_avg;
+	FILE *f;
 
-	for (i = 0; i < 50; i++)
+	pid = fork();
+	if (pid < 0) exit(1);
+	if (pid > 1)
+	{
+		f = fopen("aids.pid", "r");
+		if (f != NULL)
+		{
+			printf("Pid file exists! Not starting server...\n");
+			fclose(f);
+		} else
+		{
+			fclose(f);
+			f = fopen("aids.pid", "w");
+			fprintf(f, "%d\n", pid);
+			fclose(f);
+		}
+		exit(0);
+	}
+
+	while (1)
 	{
 		network_usage("en1", &traffic);
-		r = load_average(&load_avg);
-		if (r != -1)
-			printf("Usage: %.4g %.4g %.4g\n", load_avg.data[0], load_avg.data[1], load_avg.data[2]);
-		printf("Average network usage: %.4g kb/s in, %.4g kb/s out\n", traffic.in, traffic.out);
+		load_average(&load_avg);
 	}
 }
--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int kill_master(const char *);
+int eradicate(const int);
 int dispatch_from_args(int, char **);
 void do_run(void);
 
