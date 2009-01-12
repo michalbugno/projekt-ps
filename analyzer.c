09517e88f7d437857987fb56e10bf7b9e6828411
tph <antek.piechnik@gmail.com>
2009-01-12 03:17:54 +0100

    fixed the reading, changed filetypes

--- analyzer.c
+++ analyzer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include "main.h"
 #include "system.h"
 #include "network.h"
@@ -9,20 +10,23 @@ void data_analyzer(void)
 {
 	/*struct load_average load;*/
 	struct network_traffic traffic;
-	sleep(5);
+	struct stat buffer;
+	int status;
 	FILE* data_file;
 
-	printf("elo\n");
-
-	if ((data_file = fopen("data/current_traffic.dat", "r")) == NULL )
+	while(1) 
 	{
-		perror("file opening problem");
-		exit(-1);
+		sleep(5);
+		if ((data_file = fopen("data/current_traffic.dat", "r")) == NULL )
+		{
+			perror("file opening problem");
+			exit(-1);
+		}
+		
+		status = stat("data/current_traffic.dat", &buffer);
+		printf("%d\n", (int)buffer.st_size);
+		fscanf(data_file, "%lf,%lf\n", &(traffic.in), &(traffic.out));
+		fclose(data_file);
+		printf("%lf,%lf\n", traffic.in, traffic.out);
 	}
-
-	fscanf(data_file, "%f,%f\n", (float *)&(traffic.in), (float *)&(traffic.out));
-	fclose(data_file);
-	printf("%g,%g\n", traffic.in, traffic.out);
-
-
 }
